2d109783ad7975f6fd0c20f7fa86a7cdec124adf
Shalinee Singh <shalinee.singh@coviam.com>
2020-06-07 01:51:34 +0530

    Updates

---
+++ precache-manifest.f530b9385ce5938bdc12b772ac446861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98007ce1168a0f05eb0a5d252c8df1b",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "c30685ffc47a50e79c7b",
+    "url": "/bookstore/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "734a110dd84cbb557fe5",
+    "url": "/bookstore/static/js/2.2733492b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookstore/static/js/2.2733492b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30685ffc47a50e79c7b",
+    "url": "/bookstore/static/js/main.0aca89b5.chunk.js"
+  },
+  {
+    "revision": "b8f275cb41bf6de45a08",
+    "url": "/bookstore/static/js/runtime-main.9ceaf300.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookstore/static/media/logo.5d5d9eef.svg"
+  }
+]);
